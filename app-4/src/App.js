854c240874e7b83485d2f00711f18b2c0fd023ab
jBoothy <jhunter.booth@gmail.com>
2022-05-27 11:09:26 -0600

    finished app-4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state={
+      usernameInput: '',
+      passwordInput: ''
+    };
+  }
+
+  unInput(input){
+    this.setState({ usernameInput: input })
+  }
+
+  passInput(pass){
+    this.setState({ passwordInput: pass})
+  }
+
+  alert(){
+    window.alert('Username: ' + this.state.usernameInput + ', Password: ' + {})
+  }
+  render(){
+    return(
+      <div className='App-header'>
+      <h4>Submit Username and Password</h4>
+        <input className='username' type='text' onChange={(e)=>this.unInput(e.target.value)}></input>
+        <input className='password' type='password' onChange={(e)=>this.passInput(e.target.value)}></input>
+        <button className='submitButton' onClick={()=>window.alert('Username: '+this.state.usernameInput+', Password: '+this.state.passwordInput)}>Submit</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
