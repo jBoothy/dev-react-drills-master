e45487fbd79eb89572aaaea27a399ef0e0785770
jBoothy <jhunter.booth@gmail.com>
2022-05-27 12:05:44 -0600

    wip

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+constructor(){
+  super()
+
+  this.state={
+    userInput: '',
+    listOfChores: []
+  }
+}
+
+handleChange(input){
+  this.setState({ userInput: input })
+}
+
+pushChores(userInput){
+  let listOfChores = []
+  for(let i=0;i<userInput;i++){
+    listOfChores.push(userInput[i])
+  }
+  return listOfChores
+}
+
+render(){
+  return(
+    <div className='App'>
+      <h2>The Honey-do List</h2>
+      <input className='inputLine' onChange={(e)=>handleChange(e.target.value)}></input>
+      <button className='submitBtn' onClick={()=>this.state.userInput}>SUBMIT THAT ISH</button>
+      <span className='resultsBox'>Stuff you now have to do: {JSON.stringify(this.state.listOfChores, null, 10)}</span>
     </div>
   );
+}
 }
 
 export default App;
