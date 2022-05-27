5b18ae53aba86d18405d67f667f5f0eb4c057451
jBoothy <jhunter.booth@gmail.com>
2022-05-27 11:28:13 -0600

    finished app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bulls from '../src/images/bulls.jpeg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+constructor(){
+  super();
+
+  // this.state={
+  //   image: 'http://www.trbimg.com/img-51e06949/turbine/topic-orspt000164'
+  // };
+}
+
+render(){
+  return(
+    <div className='parentDiv'>
+      <img className='randoImage' src={bulls}/>
     </div>
-  );
+  )
+}
 }
 
 export default App;
